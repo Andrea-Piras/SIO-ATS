b47dc4aff8840943e34c9d62e89e6eb880ffb919
Andrea-Piras <33462003+Andrea-Piras@users.noreply.github.com>
2017-12-07 15:25:55 +0100

    Add files via upload

---
+++ web/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<?php include('head.php'); ?>
+
+				<body>
+
+	                <?php include('navbar.php'); ?>
+
+                    <h1>Azienda Ospedaliera Piras</h1>
+
+					<h2>Benvenuti nel mio primo SIO (Sistema Informativo Ospedaliero).</h2>  
+
+				    <p>In questo sito potrete compilare la vostra cartella elettronica e visualizzare gli esami che avete eseguito nella nostra struttura.</p>
+                    <h3 class="my"> Hope you like it. </h3>
+
+	<script src="script.js"></script>
+<?php include('script.php'); ?>
+
+</body></body> 
+
+</html>
